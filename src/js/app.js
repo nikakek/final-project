62068868f34f4e5da118360f8021cb80be47ddef
nikakek <nikakekenadze01@gmail.com>
2023-05-11 23:57:16 +0400

    section

--- src/js/app.js
+++ src/js/app.js
@@ -11,3 +11,17 @@ function showSlide(n) {
 setInterval(() => {
   showSlide(currentSlide + 1);
 }, 5000);
+
+const links = document.querySelectorAll(".clickButtons a");
+
+links.forEach(link => {
+  link.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    links.forEach(link => {
+      link.style.backgroundColor = "";
+    });
+
+    link.style.backgroundColor = "#15263E";
+  });
+});
