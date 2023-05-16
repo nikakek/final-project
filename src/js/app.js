7ead4df3105c21705cb0394a9312e796942d2384
nikakek <nikakekenadze01@gmail.com>
2023-05-16 23:37:08 +0400

    cards

--- src/js/app.js
+++ src/js/app.js
@@ -44,83 +44,96 @@ for (var i = 0; i < cards.length; i++) {
     var paragraph = document.createElement('p');
     paragraph.innerHTML = 'New Paragraph ' + (this.getAttribute('data-index') || '');
     
-    // Custom texts for each card
+   // cards
     if (this.id === 'card1') {
       heading.innerHTML = 'Graphichs Design';
       paragraph.innerHTML = 'Lorem ipm dolor amet, consecte adipiing lit Sunt sed ad possimus magm maiores.Ipsa dolor';
 
       heading.style.color = 'white';
-      heading.style.fontSize = '22px';
+      heading.style.fontSize = '18px';
       heading.style.marginTop = '100px';
       paragraph.style.color = '#ECECEC';
-      paragraph.style.fontSize = '14px';
-      
-      
-      
+      paragraph.style.fontSize = '12px';
+      paragraph.style.width = '200px';
+      paragraph.style.margin = 'auto';
 
     } else if (this.id === 'card2') {
       heading.innerHTML = 'Best Service';
       paragraph.innerHTML = 'Lorem ipm dolor amet, consecte adipiing lit Sunt sed ad possimus magm maiores.Ipsa dolor';
 
       heading.style.color = 'white';
-      heading.style.fontSize = '22px';
+      heading.style.fontSize = '18px';
       heading.style.marginTop = '100px';
       paragraph.style.color = '#ECECEC';
-      paragraph.style.fontSize = '14px';
+      paragraph.style.fontSize = '12px';
+      paragraph.style.width = '200px';
+      paragraph.style.margin = 'auto';
     }else if (this.id === 'card3') {
       heading.innerHTML = 'Best Service';
       paragraph.innerHTML = 'Lorem ipm dolor amet, consecte adipiing lit Sunt sed ad possimus magm maiores.Ipsa dolor';
 
       heading.style.color = 'white';
-      heading.style.fontSize = '22px';
+      heading.style.fontSize = '18px';
       heading.style.marginTop = '100px';
       paragraph.style.color = '#ECECEC';
-      paragraph.style.fontSize = '14px';
+      paragraph.style.fontSize = '12px';
+      paragraph.style.width = '200px';
+      paragraph.style.margin = 'auto';
     }else if (this.id === 'card4') {
       heading.innerHTML = 'Pixel Perfect';
       paragraph.innerHTML = 'Lorem ipm dolor amet, consecte adipiing lit Sunt sed ad possimus magm maiores.Ipsa dolor';
 
       heading.style.color = 'white';
-      heading.style.fontSize = '22px';
+      heading.style.fontSize = '18px';
       heading.style.marginTop = '100px';
       paragraph.style.color = '#ECECEC';
-      paragraph.style.fontSize = '14px';
+      paragraph.style.fontSize = '12px';
+      paragraph.style.width = '200px';
+      paragraph.style.margin = 'auto';
     }else if (this.id === 'card5') {
       heading.innerHTML = 'Unique Ideas';
       paragraph.innerHTML = 'Lorem ipm dolor amet, consecte adipiing lit Sunt sed ad possimus magm maiores.Ipsa dolor';
 
       heading.style.color = 'white';
-      heading.style.fontSize = '22px';
+      heading.style.fontSize = '18px';
       heading.style.marginTop = '100px';
       paragraph.style.color = '#ECECEC';
-      paragraph.style.fontSize = '14px';
+      paragraph.style.fontSize = '12px';
+      paragraph.style.width = '200px';
+      paragraph.style.margin = 'auto';
     }else if (this.id === 'card6') {
       heading.innerHTML = 'Design Analysis';
       paragraph.innerHTML = 'Lorem ipm dolor amet, consecte adipiing lit Sunt sed ad possimus magm maiores.Ipsa dolor';
 
       heading.style.color = 'white';
-      heading.style.fontSize = '22px';
+      heading.style.fontSize = '18px';
       heading.style.marginTop = '100px';
       paragraph.style.color = '#ECECEC';
-      paragraph.style.fontSize = '14px';
+      paragraph.style.fontSize = '12px';
+      paragraph.style.width = '200px';
+      paragraph.style.margin = 'auto';
     }else if (this.id === 'card7') {
       heading.innerHTML = 'Fully Managed';
       paragraph.innerHTML = 'Lorem ipm dolor amet, consecte adipiing lit Sunt sed ad possimus magm maiores.Ipsa dolor';
 
       heading.style.color = 'white';
-      heading.style.fontSize = '22px';
+      heading.style.fontSize = '18px';
       heading.style.marginTop = '100px';
       paragraph.style.color = '#ECECEC';
-      paragraph.style.fontSize = '14px';
+      paragraph.style.fontSize = '12px';
+      paragraph.style.width = '200px';
+      paragraph.style.margin = 'auto';
     }else if (this.id === 'card8') {
       heading.innerHTML = 'Helpful Support';
       paragraph.innerHTML = 'Lorem ipm dolor amet, consecte adipiing lit Sunt sed ad possimus magm maiores.Ipsa dolor';
 
       heading.style.color = 'white';
-      heading.style.fontSize = '22px';
+      heading.style.fontSize = '18px';
       heading.style.marginTop = '100px';
       paragraph.style.color = '#ECECEC';
-      paragraph.style.fontSize = '14px';
+      paragraph.style.fontSize = '12px';
+      paragraph.style.width = '200px';
+      paragraph.style.margin = 'auto';
     }
     // Add more conditions for other cards
     
